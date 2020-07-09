baa42b3254f6dab96a52dcf9927032fa1c47f511
LuxerHype <luis.guaring@campusucc.edu.co>
2020-07-09 14:51:48 -0500

    Instrucciones de uso del Drag n Drop
    Se añaden un panel canvas con texto, modificaciones en el Script de player y uno nuevo llamado NonPlayerCharacter

--- prueba jungle/Assets/Scripts/PlayerController.cs
+++ prueba jungle/Assets/Scripts/PlayerController.cs
@@ -45,30 +45,42 @@ public class PlayerController : MonoBehaviour
         animator.SetFloat("Look Y", lookDirection.y);
         animator.SetFloat("Speed", move.magnitude);
 
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, lookDirection, 0.5f, LayerMask.GetMask("Kids"));
+            if (hit.collider != null)
+            {
+                NonPlayerCharacter character = hit.collider.GetComponent<NonPlayerCharacter>();
+                if (character != null)
+                {
+                    character.DisplayDialog();
+                }
+            }
+        }
+
         Physics2D.queriesStartInColliders = false;
 
         if (Input.GetKeyDown(KeyCode.C))
         {
-            RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.right * 0.2f, lookDirection, 1.5f, LayerMask.GetMask("NPC"));
-            if (hit.collider != null)
-            { 
-                box = hit.collider.gameObject;
-                box.GetComponent<FixedJoint2D>().connectedBody = this.GetComponent<Rigidbody2D>();
-                box.GetComponent<FixedJoint2D>().enabled = true;
-                box.GetComponent<GarbagePull>().beingPushed = true;
-                transportGarbage = true;
-            }
+          RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.right * 0.2f, lookDirection, 1.5f, LayerMask.GetMask("NPC"));
+         if (hit.collider != null)
+        {
+          box = hit.collider.gameObject;
+         box.GetComponent<FixedJoint2D>().connectedBody = this.GetComponent<Rigidbody2D>();
+         box.GetComponent<FixedJoint2D>().enabled = true;
+         box.GetComponent<GarbagePull>().beingPushed = true;
+         transportGarbage = true;
+          }
         }
         else if (Input.GetKeyUp(KeyCode.X) && (transportGarbage))
         {
-            
-            box.GetComponent<FixedJoint2D>().enabled = false;
-            box.GetComponent<GarbagePull>().beingPushed = false;
-            box = null;
-            transportGarbage = false;
+
+          box.GetComponent<FixedJoint2D>().enabled = false;
+         box.GetComponent<GarbagePull>().beingPushed = false;
+         box = null;
+         transportGarbage = false;
         }
-        else {
-            Physics2D.queriesStartInColliders = false;
+        else { 
         }
     }
 
