69710670b6fce52543236e2ac5a134abd53be3b0
DactoOscar <oscar.dacto@campusucc.edu.co>
2020-07-09 19:05:38 -0500

    Borrado de plugin GitHub
    Se eliminó el plugin github
    Se organizó las carpetas prefabs

---
+++ prueba jungle/Assets/Scripts/Game/Item2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item2 : MonoBehaviour
+{
+
+    public int valor2 = 7;
+
+    public void MostrarValor2()
+    {
+        print(valor2);
+    }
+
+    public int getValor()
+    {
+       return valor2;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
