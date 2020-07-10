3114f76677514ae8ac13e1dbda0ea6abb7a8b26b
DactoOscar <oscar.dacto@campusucc.edu.co>
2020-07-09 19:10:01 -0500

    Revert "Revert "Borrado de plugin GitHub""
    This reverts commit 7cf89dc55af2f1cbd92e3b70f736f909eee52b77.

---
+++ prueba jungle/Assets/Scripts/Game/Collectable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    public GameObject reto1;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        reto1.SetActive(true);
+        Time.timeScale = 0f;
+        //Debug.Log("Object that entered the trigger : " + other);
+        //Destroy(gameObject);
+        //PlayerController controller = other.GetComponent<PlayerController>();
+
+        //if (controller != null)
+        //{
+        //    if (controller.health < controller.maxHealth)
+        //    {
+        //        controller.ChangeHealth(1);
+        //        Destroy(gameObject);
+        //    }
+        //}
+    }
+
+    
+}
